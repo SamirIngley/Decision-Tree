8a755240c11d65f2e2dc325b986cbd250e9701b6
Jonasz Karwacki <jonaszakr@gmail.com>
2020-07-17 13:34:57 -0700

    Update

--- index_GOJS.js
+++ index_GOJS.js
@@ -31,28 +31,19 @@
         { strokeWidth: 3, stroke: "#555" }));
 
     var model = $(go.TreeModel);
-    // model.nodeDataArray =
-    // [
-    // { key: "1",              option1: "Q 1",   option2: "Q 2" },
-    // { key: "2", parent: "1", option1: "Q 3",   option2: "Q 4" },
-    // { key: "3", parent: "1", option1: "Q 3.1",   option2: "Q 4.1" },
-    // { key: "4", parent: "3", option1: "Q 5",   option2: "Q 6" },
-    // { key: "5", parent: "3", option1: "Q 7",   option2: "Q 8" },
-    // { key: "6", parent: "2", option1: "Q 3.2",   option2: "Q 4.2" }
-    // ];
+
     var dataset = {'Taste':['Salty','Spicy','Spicy','Spicy','Spicy','Sweet','Salty','Sweet','Spicy','Salty'],
            'Temperature':['Hot','Hot','Hot','Cold','Hot','Cold','Cold','Hot','Cold','Hot'],
            'Texture':['Soft','Soft','Hard','Hard','Hard','Soft','Soft','Soft','Soft','Hard'],
            'Eat':['No','No','Yes','No','Yes','Yes','No','Yes','Yes','Yes']};
 
     const df = new DataFrame(dataset, ['Taste','Temperature','Texture','Eat']);
-    model.nodeDataArray = formatTree(fit(df, 'Eat'));
-    // model.nodeDataArray = [
-    //     { key: "1",              option1: "Q 1",   option2: "Q 2", option3: "asdf" },
-    //     { key: "2", parent: "1", option1: "Q 3",   option2: "Q 4" },
-    //     { key: "3", parent: "1", option1: "Q 3.1",   option2: "Q 4.1" },
-    //     { key: "4", parent: "1", option1: "Q 3.1",   option2: "Q 4.1" },
-    // ]
-    myDiagram.model = model;
 
+    var dataset2 = []
+    for (var i = 1; i < DATA.length; i++)
+        dataset2.push(DATA[i]);
+    const df2 = new DataFrame(dataset2, DATA[0]);
+    model.nodeDataArray = formatTree(fit(df, 'Eat'));
+    //model.nodeDataArray = formatTree(fit(df2, 'Outcome'));
+    //myDiagram.model = model;
 })()
--- js/main.js
+++ js/main.js
@@ -33,12 +33,6 @@ var treeRoot = d3.hierarchy({
   ]
 });
 var treemap = d3.treemap(treeRoot);
-dataset = {'Taste':['Salty','Spicy','Spicy','Spicy','Spicy','Sweet','Salty','Sweet','Spicy','Salty'],
-       'Temperature':['Hot','Hot','Hot','Cold','Hot','Cold','Cold','Hot','Cold','Hot'],
-       'Texture':['Soft','Soft','Hard','Hard','Hard','Soft','Soft','Soft','Soft','Hard'],
-       'Eat':['No','No','Yes','No','Yes','Yes','No','Yes','Yes','Yes']};
-
-const df = new DataFrame(dataset, ['Taste','Temperature','Texture','Eat']);
 
 function entropy(df, targetClass) {
     var entropy = 0;
@@ -153,6 +147,98 @@ function formatTree(tree) {
         id += 1;
     }
     return layout;
+}
+//
+// var $ = go.GraphObject.make;
+//
+// var myDiagram =
+// $(go.Diagram, "myDiagramDiv",
+//     {
+//     "undoManager.isEnabled": true,
+//     layout: $(go.TreeLayout,
+//                 { angle: 90, layerSpacing: 35 })
+//     });
+//
+// // the template we defined earlier
+// myDiagram.nodeTemplate =
+// $(go.Node, "Horizontal",
+//     { background: "lightgrey" },
+//     $(go.TextBlock, "Option 1",
+//     { margin: 12, stroke: "blue", font: "16px sans-serif" },
+//     new go.Binding("text", "option1")),
+//     $(go.TextBlock, "Option 2",
+//     { margin: 12, stroke: "red", font: "16px sans-serif" },
+//     new go.Binding("text", "option2")),
+//
+// );
+//
+// // define a Link template that routes orthogonally, with no arrowhead
+// myDiagram.linkTemplate =
+// $(go.Link,
+//     { routing: go.Link.Orthogonal, corner: 5 },
+//     $(go.Shape, // the link's path shape
+//     { strokeWidth: 3, stroke: "#555" }));
+//
+// var model = $(go.TreeModel);
+//
+// var dataset = {'Taste':['Salty','Spicy','Spicy','Spicy','Spicy','Sweet','Salty','Sweet','Spicy','Salty'],
+//        'Temperature':['Hot','Hot','Hot','Cold','Hot','Cold','Cold','Hot','Cold','Hot'],
+//        'Texture':['Soft','Soft','Hard','Hard','Hard','Soft','Soft','Soft','Soft','Hard'],
+//        'Eat':['No','No','Yes','No','Yes','Yes','No','Yes','Yes','Yes']};
+//
+// const df = new DataFrame(dataset, ['Taste','Temperature','Texture','Eat']);
+// model.nodeDataArray = formatTree(fit(df, 'Eat'));
+
+// myDiagram.model = model;
+
+function makeVisualization() {
+    var $ = go.GraphObject.make;
+
+    var myDiagram =
+    $(go.Diagram, "myDiagramDiv",
+        {
+        "undoManager.isEnabled": true,
+        layout: $(go.TreeLayout,
+                    { angle: 90, layerSpacing: 35 })
+        });
+
+    // the template we defined earlier
+    myDiagram.nodeTemplate =
+    $(go.Node, "Horizontal",
+        { background: "lightgrey" },
+        $(go.TextBlock, "Option 1",
+        { margin: 12, stroke: "blue", font: "16px sans-serif" },
+        new go.Binding("text", "option1")),
+        $(go.TextBlock, "Option 2",
+        { margin: 12, stroke: "red", font: "16px sans-serif" },
+        new go.Binding("text", "option2")),
+
+    );
+
+    // define a Link template that routes orthogonally, with no arrowhead
+    myDiagram.linkTemplate =
+    $(go.Link,
+        { routing: go.Link.Orthogonal, corner: 5 },
+        $(go.Shape, // the link's path shape
+        { strokeWidth: 3, stroke: "#555" }));
+
+    var model = $(go.TreeModel);
+
+
+
+    const dataText = document.getElementById('dataInput').value;
+    const lines = dataText.split('\n');
+    console.log(lines);
+    const columns = lines[0].split(',');
+    var rows = [];
+
+    for (var i = 1; i < lines.length; i++)
+        rows.push(lines[i].split(','));
+    var df = new DataFrame(rows, columns);
+
+    model.nodeDataArray = formatTree(fit(df, 'Outcome'));
+
+    myDiagram.model = model;
 }
 
 const tree = {'Taste': {'Salty': {'Texture': {'Hard': 'Yes', 'Soft': 'No'}},
